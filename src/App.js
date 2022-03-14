0c9f48d7894f061714158b83a93f34e0dbf93851
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-03-14 18:29:34 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import Main from "./main.js";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> 793e072 (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Main></Main>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> 793e072 (Initialize project using Create React App)
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
+=======
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+>>>>>>> 793e072 (Initialize project using Create React App)
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
