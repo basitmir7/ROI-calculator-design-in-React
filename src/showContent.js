866ae4ff2a7926b47e0245fd5ed85c1fa45ae2e1
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2022-03-15 18:31:27 +0530

    new

---
+++ src/showContent.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const ShowContent = (component, visibility = false) => {
+  const [visible, setVisible] = useState(() => visibility);
+
+  return [visible ? component : null, () => setVisible((v) => !v)];
+};
+
+export default ShowContent;
